80271da259d9fed35e863ca3a65c26de37cf936c
Adam Balan <a-kyle@hotmail.com>
2020-07-14 17:16:46 -0600

    minor changes

--- resources/views/layouts/partials/alerts.blade.php
+++ resources/views/layouts/partials/alerts.blade.php
@@ -1,12 +1,18 @@
 @if (session('success'))
     <div class="alert alert-success alert-dismissible fade show mb-2 mt-2">
         {{ session('success') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 @endif
 
 @if (session('error'))
     <div class="alert alert-danger alert-dismissible fade show mb-2 mt-2">
         {{ session('error') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 @endif
 
