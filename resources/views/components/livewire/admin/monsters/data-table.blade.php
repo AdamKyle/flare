af83d2da0c9723419b363b390616a63bbc5fbcde
Adam Balan <a-kyle@hotmail.com>
2020-09-23 18:20:39 -0600

    Minor changes

--- resources/views/components/livewire/admin/monsters/data-table.blade.php
+++ resources/views/components/livewire/admin/monsters/data-table.blade.php
@@ -16,7 +16,7 @@
                         <input wire:model="search" class="form-control" type="text" placeholder="Search monsters...">
                     </div>
                 </div>
-                <table class="table table-bordered">
+                <table class="table table-bordered data-table">
                     <thead>
                         <tr>
                             <th>
