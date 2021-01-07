c906e39d4f274560a6587d5b97a31586b17f6bd2
Adam Balan <a-kyle@hotmail.com>
2021-01-07 15:05:22 -0700

    Added more tests

---
+++ tests/Unit/Flare/View/Livewire/Admin/Kingdom/Buildings/Partials/AtrributesTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit\Flare\View\Livewire\Admin\Kingdom\Buildings\Partials;
+
+use Livewire;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Flare\View\Livewire\Admin\Kingdoms\Buildings\Partials\Attributes;
+use App\Flare\Models\GameBuilding;
+use Tests\TestCase;
+use Tests\Traits\CreateGameBuilding;
+
+class AttributesTest extends TestCase
+{
+    use RefreshDatabase, CreateGameBuilding;
+
+    public function testTheComponentLoads() {
+        Livewire::test(Attributes::class, [
+            'gameBuilding' => $this->createGameBuilding()->toArray(),
+        ])->assertSee('Cost in Wood:');
+    }
+
+    public function testTheComponentCallsUpdate() {
+        $gameBuilding = $this->createGameBuilding();
+
+        Livewire::test(Attributes::class)->call('update', $gameBuilding->id)->assertSet('gameBuilding.name', $gameBuilding->name);
+    }
+
+    public function testNoValidation() {
+        Livewire::test(Attributes::class, [
+            'gameBuilding' => $this->createGameBuilding(),
+        ])->set('gameBuilding.is_walls', true)->call('validateInput', 'nextStep', 2);
+        
+        $this->assertNotNull(GameBuilding::where('is_walls', true)->first());
+    }
+}
