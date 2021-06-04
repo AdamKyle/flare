08e34a61c27e23fcd45e43d47deb5b3cc002f9cd
Adam Balan <a-kyle@hotmail.com>
2021-06-03 20:49:31 -0600

    cast type for core stats

--- resources/views/game/character/partials/sheet/core-stats.blade.php
+++ resources/views/game/character/partials/sheet/core-stats.blade.php
@@ -1,12 +1,12 @@
 <dl>
     <dt>Strength:</dt>
-    <dd>{{$character->str}} (Modded: {{round($characterInfo['str'])}})</dd>
+    <dd>{{$character->str}} (Modded: {{round((int) $characterInfo['str'])}})</dd>
     <dt>Durability:</dt>
-    <dd>{{$character->dur}} (Modded: {{round($characterInfo['dur'])}})</dd>
+    <dd>{{$character->dur}} (Modded: {{round((int) $characterInfo['dur'])}})</dd>
     <dt>Dexterity:</dt>
-    <dd>{{$character->dex}} (Modded: {{round($characterInfo['dex'])}})</dd>
+    <dd>{{$character->dex}} (Modded: {{round((int) $characterInfo['dex'])}})</dd>
     <dt>Charisma:</dt>
-    <dd>{{$character->chr}} (Modded: {{round($characterInfo['chr'])}})</dd>
+    <dd>{{$character->chr}} (Modded: {{round((int) $characterInfo['chr'])}})</dd>
     <dt>Intelligence:</dt>
-    <dd>{{$character->int}} (Modded: {{round($characterInfo['int'])}})</dd>
+    <dd>{{$character->int}} (Modded: {{round((int) $characterInfo['int'])}})</dd>
 </dl>
