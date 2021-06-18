58a750456b943c45fcda0daad7938fafbe9b7b0c
Adam Balan <a-kyle@hotmail.com>
2021-06-17 18:11:20 -0600

    Added 503 error page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.minimum')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body">
+                        <h1 class="card-title">Down for maintenance ...</h1>
+                        <p>I am sorry, but right now we are currently down for maintenance. This won't take long, I promise, and only happens in rare instances.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
