4f5120e31040d4b1c5f1c984eedb54f9c33566c3
Adam Balan <a-kyle@hotmail.com>
2021-06-21 16:13:29 -0600

    Minor tweak to the ino sidebar

--- resources/views/layouts/partials/sidebar/informationsidebar.blade.php
+++ resources/views/layouts/partials/sidebar/informationsidebar.blade.php
@@ -6,19 +6,16 @@
           <i class="fas fa-question-circle"></i><span class="hide-menu ml-2">Information</span>
         </a>
     </li>
-    <li class="nav-devider"></li>
     <li>
         <a href="{{route('info.page', [
             'pageName' => 'time-gates'
           ])}}"><i class="far fa-clock"></i> Time Gates</a>
     </li>
-    <li class="nav-devider"></li>
     <li>
         <a href="{{route('info.page', [
             'pageName' => 'rules'
         ])}}"><i class="fas fa-smoking-ban"></i> Core Rules</a>
     </li>
-    <li class="nav-devider"></li>
     <li>
       <a class="has-arrow" href="#" aria-expanded="false"><i class="ra ra-player"></i><span class="hide-menu">Character Information</span></a>
       <ul aria-expanded="false" class="collapse">
@@ -28,7 +25,6 @@
           <li><a href="{{route('info.page', ['pageName' => 'equipment'])}}">Equipment</a></li>
       </ul>
     </li>
-    <li class="nav-devider"></li>
     <li>
       <a class="has-arrow" href="#" aria-expanded="false"><i class="ra ra-scroll-unfurled"></i><span class="hide-menu">Map</span></a>
       <ul aria-expanded="false" class="collapse">
@@ -45,7 +41,6 @@
           <li><a href="{{route('info.page', ['pageName' => 'planes'])}}">Planes</a></li>
       </ul>
     </li>
-    <li class="nav-devider"></li>
     <li>
       <a class="has-arrow" href="#" aria-expanded="false"><i class="ra ra-guarded-tower"></i><span class="hide-menu">Kingdoms</span></a>
       <ul aria-expanded="false" class="collapse">
@@ -53,25 +48,21 @@
           <li><a href="{{route('info.page', ['pageName' => 'attacking-kingdoms'])}}">Attacking A Kingdom</a></li>
       </ul>
     </li>
-    <li class="nav-devider"></li>
     <li>
         <a href="{{route('info.page', [
             'pageName' => 'settings'
           ])}}"><i class="fas fa-user-cog"></i> Player Settings</a>
     </li>
-    <li class="nav-devider"></li>
     <li>
       <a href="{{route('info.page', [
           'pageName' => 'notifications'
         ])}}"><i class="fas fa-bell"></i> Notifications</a>
     </li>
-    <li class="nav-devider"></li>
     <li>
         <a href="{{route('info.page', [
             'pageName' => 'market-board'
           ])}}"><i class="fas fa-sign"></i> Market Board</a>
     </li>
-    <li class="nav-devider"></li>
     <li>
         <a class="has-arrow" href="#" aria-expanded="false"><i class="ra ra-anvil"></i><span class="hide-menu">Crafting/Enchanting</span></a>
         <ul aria-expanded="false" class="collapse">
