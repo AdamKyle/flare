e1244bbb8f59acfde9f03287612c3b8b3fdb64e3
Adam Balan <a-kyle@hotmail.com>
2021-06-22 23:58:34 -0600

    implemented the move timeout modifier.

--- app/Game/Maps/Listeners/MoveTimeOutListener.php
+++ app/Game/Maps/Listeners/MoveTimeOutListener.php
@@ -2,12 +2,16 @@
 
 namespace App\Game\Maps\Listeners;
 
+use App\Flare\Models\Character;
 use App\Game\Maps\Events\MoveTimeOutEvent;
 use App\Game\Maps\Events\ShowTimeOutEvent;
 use App\Game\Maps\Jobs\MoveTimeOutJob;
 
 class MoveTimeOutListener
 {
+
+    private $time;
+
     /**
      * Handle the event.
      *
@@ -17,7 +21,8 @@ class MoveTimeOutListener
     public function handle(MoveTimeOutEvent $event)
     {
         $character = $event->character;
-        
+        $this->time = $event->timeOut;
+
         if ($event->timeOut !== 0) {
             $timeOut = now()->addMinutes($event->timeOut);
 
@@ -31,7 +36,9 @@ class MoveTimeOutListener
             MoveTimeOutJob::dispatch($character)->delay($timeOut);
         } else {
 
-            $timeOut = now()->addSeconds(10);
+            $this->time = 10 - (10 * $this->findMovementTimeReductions($character));
+
+            $timeOut = now()->addSeconds($this->time);
 
             $character->update([
                 'can_move'          => false,
@@ -39,10 +46,22 @@ class MoveTimeOutListener
             ]);
 
             $character = $character->refresh();
-            
+
             MoveTimeOutJob::dispatch($character)->delay($timeOut);
         }
-        
-        broadcast(new ShowTimeOutEvent($event->character->user, true, false, $event->timeOut, $event->setSail));
+
+        broadcast(new ShowTimeOutEvent($event->character->user, true, false, $this->time, $event->setSail));
+    }
+
+    protected function findMovementTimeReductions(Character $character) {
+        $skill = $character->skills->filter(function($skill) {
+            return $skill->reduces_movement_time;
+        })->first();
+
+        if (is_null($skill)) {
+            return 0;
+        }
+
+        return $skill->move_time_out_mod;
     }
 }
