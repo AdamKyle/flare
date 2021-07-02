b1a17031b5c6a26dbbcdfb8223a83fd8310454d6
Adam Balan <a-kyle@hotmail.com>
2021-07-02 16:42:37 -0600

    Fixes for tests

--- app/Game/Core/Controllers/ItemsController.php
+++ app/Game/Core/Controllers/ItemsController.php
@@ -2,17 +2,22 @@
 
 namespace App\Game\Core\Controllers;
 
-use App\Flare\Models\Item;
 use App\Http\Controllers\Controller;
+use App\Flare\Models\Adventure;
+use App\Flare\Models\Item;
+use App\Flare\Models\Location;
+use App\Flare\Models\Monster;
+use App\Flare\Models\Quest;
 
 class ItemsController extends Controller {
-
-    public function __construct() {
-    }
 
     public function show(Item $item) {
         return view('game.items.item', [
             'item' => $item,
+            'monster'   => Monster::where('quest_item_id', $item->id)->first(),
+            'quest'     => Quest::where('item_id', $item->id)->first(),
+            'location'  => Location::where('quest_reward_item_id', $item->id)->first(),
+            'adventure' => Adventure::where('reward_item_id', $item->id)->first(),
         ]);
     }
 }
--- tests/Feature/Game/Quests/Controllers/QuestsControllerTest.php
+++ tests/Feature/Game/Quests/Controllers/QuestsControllerTest.php
@@ -2,16 +2,19 @@
 
 namespace Tests\Feature\Game\Quests\Controllers;
 
+use App\Flare\Models\Npc;
+use App\Flare\Values\NpcCommandTypes;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Flare\Values\NpcTypes;
 use Tests\Setup\Character\CharacterFactory;
 use Tests\TestCase;
+use Tests\Traits\CreateItem;
 use Tests\Traits\CreateNpc;
 use Tests\Traits\CreateQuest;
 
 class QuestsControllerTest extends TestCase
 {
-    use RefreshDatabase, CreateQuest, CreateNpc;
+    use RefreshDatabase, CreateQuest, CreateNpc, CreateItem;
 
     private $character;
 
@@ -26,12 +29,19 @@ class QuestsControllerTest extends TestCase
             'character_id' => $this->character->getCharacter()->id,
             'quest_id'     => $this->createQuest([
                 'name'   => 'Sample',
+                'item_id' => $this->createItem()->id,
                 'npc_id' => $this->createNpc([
                     'type'        => NpcTypes::QUEST_GIVER,
                     'game_map_id' => $this->character->getCharacter()->map->game_map_id,
                 ])->id,
                 'unlocks_skill'   => false,
             ])->id,
+        ]);
+
+        Npc::first()->commands()->create([
+            'npc_id'       => Npc::first()->id,
+            'command'      => 'Command',
+            'command_type' => NpcCommandTypes::QUEST,
         ]);
     }
 
