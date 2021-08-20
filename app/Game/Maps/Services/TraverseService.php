ec0a06863420dba24539ce589576e2cbb2449fb8
Adam Balan <a-kyle@hotmail.com>
2021-08-19 20:56:59 -0600

    Fixed monster list

--- app/Game/Maps/Services/TraverseService.php
+++ app/Game/Maps/Services/TraverseService.php
@@ -187,7 +187,13 @@ class TraverseService {
     protected function updateActions(int $mapId, Character $character) {
         $user      = $character->user;
         $character = new Item($character, $this->characterAttackTransformer);
-        $monsters  = new Collection(Monster::where('published', true)->where('game_map_id', $mapId)->orderBy('max_level', 'asc')->get(), $this->monsterTransformer);
+        $monsters  = new Collection(
+            Monster::where('published', true)
+                   ->where('game_map_id', $mapId)
+                   ->where('is_celestial_entity', false)
+                   ->orderBy('max_level', 'asc')->get(),
+                   $this->monsterTransformer
+        );
 
         $character = $this->manager->createData($character)->toArray();
         $monster   = $this->manager->createData($monsters)->toArray();
