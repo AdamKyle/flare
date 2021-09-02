2d5fcbb6d4fad588167967ac25e57b0d7130bd11
adambalan <a-kyle@hotmail.com>
2021-09-02 12:31:26 -0700

    Final changes before release

--- app/Console/Commands/UpdateAllCharacters.php
+++ app/Console/Commands/UpdateAllCharacters.php
@@ -3,9 +3,12 @@
 namespace App\Console\Commands;
 
 use App\Flare\Models\Character;
+use App\Flare\Models\GameSkill;
 use App\Flare\Models\Item;
+use App\Flare\Models\ItemAffix;
 use App\Flare\Models\MarketHistory;
 use App\Flare\Models\Notification;
+use App\Flare\Models\Skill;
 use App\Flare\Values\BaseStatValue;
 use App\Flare\Values\CharacterClassValue;
 use Carbon\Carbon;
@@ -49,5 +52,18 @@ class UpdateAllCharacters extends Command
         $this->call('characters:assign-new-stats');
         $this->call('characters:assign-inventory-sets');
         $this->call('give:skills');
+
+        $this->line('making sure Alchemy is locked ...');
+        // Make sure all character skill's Alchemy are locked.
+        Skill::where('game_skill_id', GameSkill::where('name', 'Alchemy')->first()->id)->update([
+            'is_locked' => true,
+        ]);
+        $this->line('Updating Item prices ...');
+        // Make sure all items and affixes are priced properly:
+        Item::where('cost', '<=', 1000)->where('usable', false)->update(['can_drop' => true]);
+        Item::where('cost', '>', 1000)->where('usable', false)->update(['can_drop' => false]);
+        $this->line('Updating Item Affixes prices ...');
+        ItemAffix::where('cost', '<=', 1500)->update(['can_drop' => true]);
+        ItemAffix::where('cost', '>', 1500)->update(['can_drop' => false]);
     }
 }
