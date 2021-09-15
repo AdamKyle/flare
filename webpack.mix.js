39ef6356445dc36ee6181b864caeef8826aabe4e
Adam Balan <a-kyle@hotmail.com>
2021-09-14 18:03:40 -0600

    made the front end very tiny.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,15 +11,13 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.react('resources/js/app.js', 'public/js')
-   .react('resources/js/helpers/market-board.js', 'public/js')
-   .react('resources/js/helpers/kingdom-unit-movement.js', 'public/js')
-   .react('resources/js/helpers/admin-chat-messages.js', 'public/js')
-   .react('resources/js/helpers/admin-site-stats-components.js', 'public/js')
-   .react('resources/js/helpers/character-boons.js', 'public/js')
-   .react('resources/js/helpers/admin-statistics.js', 'public/js')
+mix.react('resources/js/app.js', 'public/js').extract()
+   .react('resources/js/helpers/kingdom-unit-movement.js', 'public/js').extract()
+   .react('resources/js/helpers/admin-chat-messages.js', 'public/js').extract()
+   .react('resources/js/helpers/admin-site-stats-components.js', 'public/js').extract()
+   .react('resources/js/helpers/character-boons.js', 'public/js').extract()
+   .react('resources/js/helpers/admin-statistics.js', 'public/js').extract()
    .sass('resources/sass/app.scss', 'public/css')
    .sourceMaps()
    .version()
-   .sourceMaps()
    .browserSync('127.0.0.1:8000');
