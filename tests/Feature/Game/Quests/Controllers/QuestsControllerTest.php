309288d4b89cc98cfdf5d0d33bae6eebc77e3d4f
Adam Balan <a-kyle@hotmail.com>
2021-10-26 19:29:38 -0600

    Added more tests back.
    - So far we are at 32% coverage.

---
+++ tests/Feature/Game/Quests/Controllers/QuestsControllerTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Feature\Game\Quests\Controllers;
+
+use App\Flare\Models\Npc;
+use App\Flare\Values\NpcCommandTypes;
+use App\Game\Skills\Values\SkillTypeValue;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Flare\Values\NpcTypes;
+use Tests\Setup\Character\CharacterFactory;
+use Tests\TestCase;
+use Tests\Traits\CreateGameSkill;
+use Tests\Traits\CreateItem;
+use Tests\Traits\CreateNpc;
+use Tests\Traits\CreateQuest;
+
+class QuestsControllerTest extends TestCase
+{
+    use RefreshDatabase, CreateQuest, CreateNpc, CreateItem, CreateGameSkill;
+
+    private $character;
+
+    private $completedQuest;
+
+    public function setUp(): void {
+        parent::setUp();
+
+        $gameSkill = $this->createGameSkill([
+            'type'      => SkillTypeValue::ALCHEMY,
+            'is_locked' => true,
+        ]);
+
+        $this->character = (new CharacterFactory)
+            ->createBaseCharacter()
+            ->givePlayerLocation()
+            ->assignSkill($gameSkill, 1, false);
+
+        $this->completedQuest = $this->createCompletedQuest([
+            'character_id' => $this->character->getCharacter(false)->id,
+            'quest_id'     => $this->createQuest([
+                'name'   => 'Sample',
+                'item_id' => $this->createItem()->id,
+                'npc_id' => $this->createNpc([
+                    'type'        => NpcTypes::QUEST_GIVER,
+                    'game_map_id' => $this->character->getCharacter(false)->map->game_map_id,
+                ])->id,
+                'unlocks_skill'   => true,
+                'unlocks_skill_type' => SkillTypeValue::ALCHEMY
+            ])->id,
+        ]);
+
+        Npc::first()->commands()->create([
+            'npc_id'       => Npc::first()->id,
+            'command'      => 'Command',
+            'command_type' => NpcCommandTypes::QUEST,
+        ]);
+    }
+
+    public function testVisitCompleteQuestsPage() {
+        $character = $this->character->getCharacter(false);
+
+        $this->actingAs($character->user)->visitRoute('completed.quests', [
+            'user' => $character->user->id,
+        ])->see('Completed Quests');
+    }
+
+    public function testVisitCompletedQuestPage() {
+        $character = $this->character->getCharacter(false);
+
+        $this->actingAs($character->user)->visitRoute('completed.quest', [
+            'character'       => $character->id,
+            'questsCompleted' => $this->completedQuest->id,
+        ])->see($this->completedQuest->quest->name);
+    }
+}
