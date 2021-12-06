5c0063785f44f94851d1e7aa63c52754e99aa715
Adam Balan <a-kyle@hotmail.com>
2021-12-06 16:44:44 -0700

    Added an alert to the market sell page

--- resources/views/game/core/market/sell.blade.php
+++ resources/views/game/core/market/sell.blade.php
@@ -10,6 +10,16 @@
                 color="primary"
             ></x-core.page-title>
 
+            <x-core.alerts.info-alert title="ATTN!">
+                <p>
+                    You can only sell enchanted items or <a href="/information/usable-items">alchemical items</a> on the market board.
+                </p>
+                <p>
+                    If you are seeing no items show up in the table, chances are you have no enchanted gear
+                    or alchemical items to sell.
+                </p>
+            </x-core.alerts.info-alert>
+
             <div id="sell-items-on-market">
         </div>
     </div>
