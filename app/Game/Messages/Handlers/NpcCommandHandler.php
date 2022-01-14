3e0de39aa492bd060d3dd747319633fb21f302d9
Adam Balan <a-kyle@hotmail.com>
2022-01-14 14:07:58 -0700

    Fixes and refactorings

--- app/Game/Messages/Handlers/NpcCommandHandler.php
+++ app/Game/Messages/Handlers/NpcCommandHandler.php
@@ -64,7 +64,6 @@ class NpcCommandHandler {
      * @param int $type
      * @param Npc $npc
      * @param User $user
-     * @return PendingBroadcast
      * @throws Exception
      */
     public function handleForType(int $type, Npc $npc, User $user) {
@@ -73,18 +72,24 @@ class NpcCommandHandler {
         $messageType = null;
 
         if ($user->character->is_dead) {
-            return broadcast(new ServerMessageEvent($user, $this->npcServerMessageBuilder->build('dead', $npc), true));
+            broadcast(new ServerMessageEvent($user, $this->npcServerMessageBuilder->build('dead', $npc), true));
+
+            return;
         }
 
         if (!$user->character->can_adventure) {
-            return broadcast(new ServerMessageEvent($user, $this->npcServerMessageBuilder->build('adventuring', $npc), true));
+            broadcast(new ServerMessageEvent($user, $this->npcServerMessageBuilder->build('adventuring', $npc), true));
+
+            return;
         }
 
         if ($npc->must_be_at_same_location) {
             $character = $user->character;
 
             if ($character->x_position !== $npc->x_position && $character->y_position !== $npc->y_position) {
-                return broadcast(new ServerMessageEvent($user, $this->npcServerMessageBuilder->build('location', $npc), true));
+                broadcast(new ServerMessageEvent($user, $this->npcServerMessageBuilder->build('location', $npc), true));
+
+                return;
             }
         }
 
@@ -94,98 +99,20 @@ class NpcCommandHandler {
                 $messageType = 'quest_complete';
             } else {
                 $messageType = 'no_quests';
-            };
+            }
         }
 
         if (!is_null($message) && !is_null($messageType)) {
             broadcast(new GlobalMessageEvent($message));
 
-            return broadcast(new ServerMessageEvent($user, $this->npcServerMessageBuilder->build($messageType, $npc), true));
-        } else if (!is_null($messageType)) {
-            return broadcast(new ServerMessageEvent($user, $this->npcServerMessageBuilder->build($messageType, $npc), true));
+            broadcast(new ServerMessageEvent($user, $this->npcServerMessageBuilder->build($messageType, $npc), true));
+
+            return;
         }
-    }
 
-
-
-    /**
-     * Handles taking the kingdom.
-     *
-     * @param User $user
-     * @param Npc $npc
-     * @return bool
-     */
-    protected function handleTakingKingdom(User $user, Npc $npc): bool {
-        $character      = $user->character;
-        $characterX     = $character->map->character_position_x;
-        $characterY     = $character->map->character_position_y;
-        $characterMapId = $character->map->game_map_id;
-        $tookKingdom    = false;
-
-        $kingdom = Kingdom::whereNull('character_id')
-                          ->where('x_position', $characterX)
-                          ->where('y_position', $characterY)
-                          ->where('game_map_id', $characterMapId)
-                          ->where('npc_owned', true)
-                          ->first();
-
-        dump($kingdom);
-
-//        if (is_null($kingdom)) {
-//            broadcast(new ServerMessageEvent($user, $this->npcServerMessageBuilder->build('cannot_have', $npc), true));
-//            dump('cannot have....');
-//        } else {
-//            dump('I am in here ...');
-//            $gold         = $character->gold;
-//            $kingdomCount = $character->kingdoms()->where('game_map_id', $character->map->game_map_id)->count();
-//            $cost         = ($kingdomCount * self::KINGDOM_COST);
-//
-//            if ($gold < $cost) {
-//                broadcast(new ServerMessageEvent($user, $this->npcServerMessageBuilder->build('not_enough_gold', $npc), true));
-//
-//                return false;
-//            } else {
-//                $character->update([
-//                    'gold' => $gold - $cost,
-//                ]);
-//
-//                event(new UpdateTopBarEvent($character->refresh()));
-//            }
-//
-//            $kingdom->update([
-//                'character_id' => $character->id,
-//                'npc_owned'    => false,
-//                'last_walked'  => now(),
-//            ]);
-//
-//            foreach ($kingdom->buildings as $building) {
-//                $passive = PassiveSkill::where('name', $building->name)->where('type', PassiveSkillTypeValue::UNLOCKS_BUILDING)->first();
-//
-//                if (!is_null($passive)) {
-//                    $characterPassive = $character->passiveSkills()->where('passive_skill_id', $passive->id)->first();
-//
-//                    if (!is_null($characterPassive)) {
-//                        if ($characterPassive->is_locked && $characterPassive->level < 1) {
-//                            $building->update([
-//                                'is_locked' => true,
-//                            ]);
-//
-//                            event(new ServerMessageEvent($character->user, $building->name . ' has been locked, as you do not meet the passive skill requirements.'));
-//                        }
-//                    }
-//                }
-//            }
-//
-//            $this->addKingdomToCache($character->refresh(), $kingdom->refresh());
-//
-//            event(new AddKingdomToMap($character));
-//            event(new UpdateGlobalMap($character));
-//            event(new UpdateNPCKingdoms($character->map->gameMap));
-//
-//            $tookKingdom = true;
-//        }
-
-        return $tookKingdom;
+        if (!is_null($messageType)) {
+            broadcast(new ServerMessageEvent($user, $this->npcServerMessageBuilder->build($messageType, $npc), true));
+        }
     }
 
     protected function handleQuest($user, $npc): bool {
