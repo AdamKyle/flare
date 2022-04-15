5024402c151c680d307d368fc7a436ae7093ea2f
Adam Balan <a-kyle@hotmail.com>
2022-04-15 17:35:34 -0600

    Started to redo the items page

--- resources/views/game/items/item.blade.php
+++ resources/views/game/items/item.blade.php
@@ -1,28 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="w-full lg:w-3/4 m-auto mt-10 mb-10">
-        <x-core.page-title-slot
-            route="{{url()->previous()}}"
-            link="Back"
-            color="success"
-        >
-            <x-item-display-color :item="$item" />
-        </x-core.page-title-slot>
-        <hr />
-        @if ($item->market_sellable)
-            <div class="alert alert-info mt-1 mb-2">This item can be sold on the market.</div>
-        @endif
-
-        @include('game.items.partials.item', [
-            'item'      => $item,
-            'effects'   => $effects,
-            'monster'   => $monster,
-            'quest'     => $quest,
-            'location'  => $location,
-            'adventure' => $adventure,
-            'skills'    => $skills,
-            'skill'     => $skill,
-        ])
-    </div>
+    @dump([
+        'item'      => $item,
+        'effects'   => $effects,
+        'monster'   => $monster,
+        'quest'     => $quest,
+        'location'  => $location,
+        'adventure' => $adventure,
+        'skills'    => $skills,
+        'skill'     => $skill,
+    ])
 @endsection
--- resources/views/game/items/partials/item-quest-section.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="col-md-6">
-    @include('game.items.partials.item-quest-details')
-</div>
