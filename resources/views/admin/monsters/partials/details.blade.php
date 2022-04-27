74fb770efbe080a852ba4174072dcded3372280f
Adam Balan <a-kyle@hotmail.com>
2022-04-27 14:40:36 -0600

    Fixed the monster layout.

--- resources/views/admin/monsters/partials/details.blade.php
+++ resources/views/admin/monsters/partials/details.blade.php
@@ -1,6 +1,8 @@
 <x-core.cards.card>
-    <div class="row">
-        <div class="col-md-4">
+    <div class="grid md:grid-cols-2 gap-4">
+        <div>
+            <strong>Stats</strong>
+            <div class='border-b-2 border-b-gray-300 dark:border-b-gray-600 my-3'></div>
             <dl>
                 <dt>str</dt>
                 <dd>{{number_format($monster->str)}}</dd>
@@ -19,8 +21,9 @@
                 <dt>Damage Stat</dt>
                 <dd>{{$monster->damage_stat}}</dd>
             </dl>
-            <hr />
-            <h5 class="mt-3">Skill Info</h5>
+            <div class='border-b-2 border-b-gray-300 dark:border-b-gray-600 my-3'></div>
+            <strong>Skills</strong>
+            <div class='border-b-2 border-b-gray-300 dark:border-b-gray-600 my-3'></div>
             <dl>
                 <dt>Accuracy</dt>
                 <dd>{{$monster->accuracy * 100}}%</dd>
@@ -32,16 +35,24 @@
                 <dd>{{$monster->dodge * 100}}%</dd>
             </dl>
         </div>
-        <div class="col-md-4">
+        <div class='block md:hidden border-b-2 border-b-gray-300 dark:border-b-gray-600 my-3'></div>
+        <div>
+            <strong>Health/Damage/AC</strong>
+            <div class='border-b-2 border-b-gray-300 dark:border-b-gray-600 my-3'></div>
             <dl>
                 <dt>Health Range</dt>
                 <dd>{{number_format(explode('-', $monster->health_range)[0])}} - {{number_format(explode('-', $monster->health_range)[1])}}</dd>
                 <dt>Attack Range</dt>
                 <dd>{{number_format(explode('-', $monster->attack_range)[0])}} - {{number_format(explode('-', $monster->attack_range)[1])}}</dd>
-                <dt>Drop Check</dt>
-                <dd>{{$monster->drop_check * 100}}%</dd>
                 <dt>AC</dt>
                 <dd>{{number_format($monster->ac)}}</dd>
+            </dl>
+            <div class='border-b-2 border-b-gray-300 dark:border-b-gray-600 my-3'></div>
+            <strong>Reward Details</strong>
+            <div class='border-b-2 border-b-gray-300 dark:border-b-gray-600 my-3'></div>
+            <dl>
+                <dt>Drop Chance</dt>
+                <dd>{{$monster->drop_check * 100}}%</dd>
                 <dt>XP</dt>
                 <dd>{{$monster->xp}}</dd>
                 <dt>Max Level<sup>*</sup></dt>
@@ -49,25 +60,51 @@
                 <dt>Gold Reward</dt>
                 <dd>{{number_format($monster->gold)}}</dd>
             </dl>
+            <p class="mt-4"><sup>*</sup> Indicates that if you are over this level, you only get 1/3<sup>rd</sup> the monster's XP</p>
         </div>
-        <div class="col-md-4">
-            <dl>
-                <dt>Spell Evasion</dt>
-                <dd>{{$monster->spell_evasion * 100}}%</dd>
-                <dt>Artifact Annulment</dt>
-                <dd>{{$monster->artifact_annulment * 100}}%</dd>
-            </dl>
-            <hr />
-            <x-core.alerts.info-alert title="Devouring Light and Devouring Darkness">
+    </div>
+    <div class="grid md:grid-cols-2 gap-4">
+        <div>
+            <div class='border-b-2 border-b-gray-300 dark:border-b-gray-600 my-3'></div>
+            <strong>Resistances</strong>
+            <div class='border-b-2 border-b-gray-300 dark:border-b-gray-600 my-3'></div>
+            <x-core.alerts.info-alert title="Info">
+                <p>All Enemies have a chance to completely annul your affixes that do damage. This percentage is also known as
+                    a "chance". The stronger the enemy the higher the chance to annul your affix damage.</p>
                 <p>
+                    There are some affixes who's damage cannot be resisted, this is known as irresistible damage. Even if an enemy's
+                    Affix Resistance is over 100% this damage cannot be resisted.
+                </p>
+                <p>
+                    There is a quest item that can make your affixes irresistible, which can then be upgraded to make your rings and
+                    artifacts irresistible. You can then further upgrade this to make spells irresistible.
+                </p>
+                <ul>
+                    <li>Casters will use 5% of their Focus as the base DC check for enemy's resistances check to see if the enemy can avoid the spell(s).</li>
+                    <li>Vampires will use 5% of their Durability as the base dc check for resistances check to see if the enemy can avoid life stealing affixes.</li>
+                </ul>
+            </x-core.alerts.info-alert>
+            <dl>
+                <dt>Affix Resistance (chance):</dt>
+                <dd>{{$monster->affix_resistance * 100}}%</dd>
+                <dt>Artifact Annulment (chance):</dt>
+                <dd>{{$monster->artifact_annulment * 100}}%</dd>
+                <dt>Spell Evasion (chance):</dt>
+                <dd>{{$monster->spell_evasion * 100}}%</dd>
+            </dl>
+        </div>
+        <div class='block md:hidden border-b-2 border-b-gray-300 dark:border-b-gray-600 my-3'></div>
+        <div>
+            <x-core.alerts.info-alert title="Devouring Light and Devouring Darkness">
+                <p class="mb-4">
                     Some monsters can void you. Much like the quest item to obtain for voiding enemies of their affixes, spells,
                     artifacts and ability to heal - a Celestial can void your affixes. This is called: Devouring Light.
                     All Celestials have a Devouring Light Percentage starting at or above 50%. Harder critters in the list start at 10% and go up.
                 </p>
-                <p>Special locations can increase this (and everything else - except drop chance) by 25% in addition to any map increases in enemy strength.</p>
-                <p>There is a special quest item you can obtain to "devoid" their void, also known as: Devouring Darkness.
+                <p class="mb-4">Special locations can increase this (and everything else - except drop chance) by 25% in addition to any map increases in enemy strength.</p>
+                <p class="mb-4">There is a special quest item you can obtain to "devoid" their void, also known as: Devouring Darkness.
                     This has a chance starting at 50% to void out their chance to void you. You can upgrade this item to upgrade its chance.</p>
-                <p>
+                <p class="mb-4">
                     Monsters in purgatory have a Devouring Darkness chance, which means they can devoid you - you cannot void them. To get around this
                     players will craft <a href="/information/holy-items">Holy Items</a> after completing a quest line. If a player becomes devoided,
                     their void (Devouring light) will not fire, much like for enemies who you devoid.
@@ -80,52 +117,17 @@
                 <dd>{{$monster->devouring_darkness_chance * 100}}%</dd>
             </dl>
         </div>
-        <p class="ml-3 mt-3">
-            <span class="text-muted" style="font-size: 12px;"><sup>*</sup> Once a character is at this level or above it, they get 1/3rd the xp</span>
-        </p>
     </div>
-    @guest
-    @elseif (auth()->user()->hasRole('Admin') && $canEdit)
-        <a href="{{route('monster.edit', [
-            'monster' => $monster->id,
-        ])}}" class="btn btn-primary mt-2">Edit Monster</a>
-    @endguest
+
 </x-core.cards.card>
-<hr />
-<x-core.cards.card-with-title title="Resistances">
-    <x-core.alerts.info-alert>
-        <p>All Enemies have a chance to completely annul your affixes that do damage. This percentage is also known as
-            a "chance". The stronger the enemy the higher the chance to annul your affix damage.</p>
-        <p>
-            There are some affixes who's damage cannot be resisted, this is known as irresistible damage. Even if an enemy's
-            Affix Resistance is over 100% this damage cannot be resisted.
-        </p>
-        <p>
-            There is a quest item that can make your affixes irresistible, which can then be upgraded to make your rings and
-            artifacts irresistible. You can then further upgrade this to make spells irresistible.
-        </p>
-        <ul>
-            <li>Casters will use 5% of their Focus as the base DC check for enemy's resistances check to see if the enemy can avoid the spell(s).</li>
-            <li>Vampires will use 5% of their Durability as the base dc check for resistances check to see if the enemy can avoid life stealing affixes.</li>
-        </ul>
-    </x-core.alerts.info-alert>
-    <dl class="mt-3">
-        <dt>Affix Resistance (chance):</dt>
-        <dd>{{$monster->affix_resistance * 100}}%</dd>
-        <dt>Artifact Annulment (chance):</dt>
-        <dd>{{$monster->artifact_annulment * 100}}%</dd>
-        <dt>Spell Evasion (chance):</dt>
-        <dd>{{$monster->spell_evasion * 100}}%</dd>
-    </dl>
-</x-core.cards.card-with-title>
-<hr />
+
 <x-core.cards.card-with-title title="Cast, Artifacts and Affixes">
     <x-core.alerts.info-alert>
-        <p>
+        <p class="mb-4">
             All monsters can cast to some degree, have artifacts, affixes and can heal. Here you will see the details corresponding to that.
             There are a couple things to keep in mind, however:
         </p>
-        <ul>
+        <ul class="mb-4 list-disc ml-[20px]">
             <li>
                 Monsters cast on their turn, same for affixes and artifacts.
             </li>
@@ -137,7 +139,7 @@
                 This is where stat reducing affixes can come in handy. You can reduce the enemy's durability so they cannot heal as much.
             </li>
         </ul>
-        <p>
+        <p class="mb-4">
             There is a quest chain you can do that rewards you at the end with a new item that will annul the enemy's affixes, artifacts, spells
             and ability to heal.
         </p>
@@ -158,12 +160,12 @@
 <hr />
 <x-core.cards.card-with-title title="Ambush & Counter">
     <x-core.alerts.info-alert>
-        <p>
+        <p class="mb-4">
             Some monsters, specifically those who live in Purgatory, will have what is known as Ambush and Counter Stats.
             Ambush allows the enemy to attempt to get the jump on you doing 2x their normal weapon attack. Counter allows the enemy to counter
             your attack at +50% to their weapon attack.
         </p>
-        <p>
+        <p class="mb-4">
             Players can also get Ambush and Counter stats on gear such as Trinkets which can be crafted only when you have access to Purgatory and copper coins.
             For more info, it is suggested players read the <a href="/information/combat">Combat docs</a> and <a href="/information/crafting">Crafting guide</a> as
             Trinkets can only be crafted.
@@ -185,14 +187,14 @@
     <hr />
     <x-core.cards.card-with-title title="Celestial Conjuration Cost">
         <x-core.alerts.info-alert>
-            <p>This is a celestial entity which can only be conjured via a special NPC. You can learn more about those <a href="/information/celestials">here</a>.</p>
-            <p>This creature will have a cost in <strong>Gold</strong> and <strong>Gold Dust</strong> and can be summoned either privately or publicly.</p>
-            <p>These Creatures can also give quest rewards when defeated, as well as other items.</p>
-            <p>Celestial entities also drop what are called <strong>Shards</strong>. These are used in <a href="/information/usable-items">Alchemy</a> in place of gold.</p>
-            <p>Celestial Entities can also spawn randomly on the map by a player, any player, just moving around. When these entities spawn - be it summoned or other wise, they
+            <p class="mb-4">This is a celestial entity which can only be conjured via a special NPC. You can learn more about those <a href="/information/celestials">here</a>.</p>
+            <p class="mb-4">This creature will have a cost in <strong>Gold</strong> and <strong>Gold Dust</strong> and can be summoned either privately or publicly.</p>
+            <p class="mb-4">These Creatures can also give quest rewards when defeated, as well as other items.</p>
+            <p class="mb-4">Celestial entities also drop what are called <strong>Shards</strong>. These are used in <a href="/information/usable-items">Alchemy</a> in place of gold.</p>
+            <p class="mb-4">Celestial Entities can also spawn randomly on the map by a player, any player, just moving around. When these entities spawn - be it summoned or other wise, they
                 spawn in random locations at which the player must then go to. If the location is a kingdom is a small chance of it doing damage to the kingdom. The chance of a beast spawning
                 is greater than the chance of it doing damage to a kingdom when it does spawn.</p>
-            <p>
+            <p class="mb-4">
                 <strong>Vampires</strong> will only do 50% damage to these creatures via their life stealing affixes and class bonus.
             </p>
         </x-core.alerts.info-alert>
--- resources/views/admin/monsters/partials/monster.blade.php
+++ resources/views/admin/monsters/partials/monster.blade.php
@@ -1,32 +1,39 @@
-<div class="row page-titles">
-    <div class="col-md-6 align-self-right">
-        <h4 class="mt-2">{{$monster->name}} ({{$monster->gameMap->name}})</h4>
-    </div>
-    <div class="col-md-6 align-self-right">
-        <a href="{{url()->previous()}}" class="btn btn-primary float-right ml-2">Back</a>
-    </div>
+<x-core.layout.info-container>
+    @php
+        $backUrl = route('monsters.list');
+
+        if (!auth()->user()->hasRole('Admin')) {
+            $backUrl = '/information/monsters';
+        }
+    @endphp
+
+    <x-core.page-title
+        title="{{$monster->name}}"
+        route="{{$backUrl}}"
+        color="success" link="Back"
+    >
+        @if (auth()->user()->hasRole('Admin'))
+            <x-core.buttons.link-buttons.primary-button
+                href="{{route('monster.edit', ['monster' => $monster->id])}}"
+                css="tw-ml-2"
+            >
+                Edit Monster
+            </x-core.buttons.link-buttons.primary-button>
+        @endif
+    </x-core.page-title>
+
+    @include('admin.monsters.partials.details', [
+        'monster' => $monster,
+        'quest'   => $quest,
+        'canEdit' => true,
+    ])
+</x-core.layout.info-container>
+
+<div class="mt-4 mb-4">
+    @if (!is_null($monster->quest_item_id))
+        <p class="mb-4"><strong>Drop Chance: </strong> {{$monster->quest_item_drop_chance * 100}}%, used in: <a href="{{route('info.page.quest', ['quest' =>$quest->id])}}">{{$quest->name}}</a></p>
+        @include('game.items.partials.item-details', ['item' => $monster->questItem])
+    @endif
 </div>
 
-@include('admin.monsters.partials.details', [
-    'monster' => $monster,
-    'quest'   => $quest,
-    'canEdit' => true,
-])
 
-@if (!is_null($monster->quest_item_id))
-    <hr />
-    <div class="row page-titles">
-        <div class="col-md-6 align-self-right">
-            <h4 class="mt-2">{{$monster->questItem->affix_name}} (Quest Item)</h4>
-            <span style="font-size: 12px;">
-                <strong>Drop Chance:</strong> {{$monster->quest_item_drop_chance * 100}}%
-                @if(!is_null($quest))
-                    , used in: <a href="{{route('info.page.quest', ['quest' =>$quest->id])}}">{{$quest->name}}</a>
-                @endif
-            </span>
-        </div>
-    </div>
-    <x-core.cards.card>
-        @include('game.items.partials.item-details', ['item' => $monster->questItem])
-    </x-core.cards.card>
-@endif
