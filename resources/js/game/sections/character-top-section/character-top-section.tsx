3816eb1f9efa819ab22416a1dddc8db9f2b0e9bf
Adam Balan <a-kyle@hotmail.com>
2022-10-09 21:34:34 -0600

    Fixed XP on top bar when the character info is closed.

--- resources/js/game/sections/character-top-section/character-top-section.tsx
+++ resources/js/game/sections/character-top-section/character-top-section.tsx
@@ -81,7 +81,7 @@ export default class CharacterTopSection extends React.Component<CharacterTopSec
                     <div className='relative top-[24px]'>
                         <div className="flex justify-between mb-1">
                             <span className="font-medium text-orange-700 dark:text-white text-xs">XP (Current Level: {this.props.character.level}/{this.props.character.max_level})</span>
-                            <span className="text-xs font-medium text-orange-700 dark:text-white">{this.props.character.xp}/100</span>
+                            <span className="text-xs font-medium text-orange-700 dark:text-white">{this.props.character.xp}/{this.props.character.max_level}</span>
                         </div>
                         <div className="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
                             <div className="bg-orange-600 h-1.5 rounded-full" style={{width: this.getXpPercentage() + '%'}}></div>
