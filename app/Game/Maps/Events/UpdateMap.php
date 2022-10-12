bf522398120fcd28787571d8baf6ac647d08aa2f
Adam Balan <a-kyle@hotmail.com>
2022-10-12 13:12:28 -0600

    Forgot to add a file

---
+++ app/Game/Maps/Events/UpdateMap.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Game\Maps\Events;
+
+use App\Game\Maps\Services\LocationService;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+Use App\Flare\Models\User;
+use App\Game\Core\Traits\KingdomCache;
+
+class UpdateMap implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels, KingdomCache;
+
+    /**
+     * @var array $mapDetails
+     */
+    public array $mapDetails;
+
+    /**
+     * @var User $user
+     */
+    public User $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param User $user
+     */
+    public function __construct(User $user) {
+        $character        = $user->character->refresh();
+        $this->mapDetails = resolve(LocationService::class)->getLocationData($character);
+        $this->user       = $user;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn(): Channel|array {
+        return new PrivateChannel('update-plane-' . $this->user->id);
+    }
+}
