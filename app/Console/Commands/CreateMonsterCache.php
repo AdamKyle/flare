18b5ef672a4131425cbeff6e5034dc2ce898f115
Adam Balan <a-kyle@hotmail.com>
2022-10-24 21:43:26 -0600

    Forgot to add command

---
+++ app/Console/Commands/CreateMonsterCache.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Flare\Services\BuildMonsterCacheService;
+use Illuminate\Console\Command;
+
+class CreateMonsterCache extends Command {
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'generate:monster-cache';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generates monsters to fight';
+
+    /**
+     * Execute the console command.
+     *
+     * @param BuildMonsterCacheService $buildMonsterCacheService
+     * @return void
+     */
+    public function handle(BuildMonsterCacheService $buildMonsterCacheService): void
+    {
+        $buildMonsterCacheService->buildCache();
+        $buildMonsterCacheService->buildCelesetialCache();
+    }
+}
