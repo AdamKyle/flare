25419b2945705c77b8e82cd711f4295502218284
Adam Balan <a-kyle@hotmail.com>
2022-11-28 17:40:13 -0700

    Minor changes

--- app/Flare/Builders/AffixAttributeBuilder.php
+++ app/Flare/Builders/AffixAttributeBuilder.php
@@ -212,8 +212,8 @@ class AffixAttributeBuilder {
     public function setSkillDetails(): array {
         return [
             'skill_name'              => $this->characterSkills[rand(0, count($this->characterSkills) - 1)]->baseSkill->name,
-            'skill_training_bonus'    => $this->getPercentage($this->percentageRange[0], $this->percentageRange[1]) / 10,
-            'skill_bonus'             => $this->getPercentage($this->percentageRange[0], $this->percentageRange[1]) / 10,
+            'skill_training_bonus'    => $this->getPercentage($this->percentageRange[0], $this->percentageRange[1]),
+            'skill_bonus'             => $this->getPercentage($this->percentageRange[0], $this->percentageRange[1]),
         ];
     }
 
