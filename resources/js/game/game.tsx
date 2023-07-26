bc20d90a13ba6cba8d4b7728cddc59faacb5e8b1
Adam Balan <a-kyle@hotmail.com>
2023-07-25 23:18:04 -0600

    Minor change

--- resources/js/game/game.tsx
+++ resources/js/game/game.tsx
@@ -148,7 +148,6 @@ export default class Game extends React.Component<GameProps, GameState> {
 
         // @ts-ignore
         this.characterTopBar.listen('Game.Core.Events.UpdateTopBarBroadcastEvent', (event: any) => {
-            console.log(this.state.character?.level, event.characterSheet.level);
             this.setState({
                 character: {...this.state.character, ...event.characterSheet}
             }, () => {
