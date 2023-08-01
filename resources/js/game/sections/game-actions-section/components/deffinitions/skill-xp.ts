8503e2f271af1eb3c3eff0bf2f9f239523d66c1b
Adam Balan <a-kyle@hotmail.com>
2023-08-01 16:09:28 -0600

    Created a crafting XP bar for the crafting sections

---
+++ resources/js/game/sections/game-actions-section/components/deffinitions/skill-xp.ts
@@ -0,0 +1,6 @@
+export default interface SkillXP {
+    next_level_xp: number;
+    current_xp: number;
+    skill_name: string;
+    level: number;
+}
---
+++ resources/js/game/sections/game-actions-section/components/types/crsfting-xp-props.ts
@@ -0,0 +1,6 @@
+import SkillXP from "../deffinitions/skill-xp";
+
+export default interface CraftingXPProps {
+
+    skill_xp: SkillXP;
+}
