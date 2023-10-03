12ce1e1d22493bf27c686a8d101fb30a8af0d1ac
Adam Balan <a-kyle@hotmail.com>
2023-10-02 20:26:37 -0600

    Removed views no longer being used

--- resources/views/game/character/partials/equipment/sections/equip/armour.blade.php
+++
@@ -1,9 +0,0 @@
-<input type="hidden" name="slot_id" value={{$slotId}} />
-<input type="hidden" name="equip_type" value={{$itemToEquip->type}} />
-<input type="hidden" name="position" value={{$itemToEquip->default_position}} />
-
-@if ($isShop)
-  <input type="hidden" name="item_id_to_buy" value={{$itemToEquip->id}} />
-@endif
-
-<p class="text-info">You cannot select the position as this has a default position.<p>
