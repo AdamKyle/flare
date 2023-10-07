25f67c4f79d3723ea9bb80b3460e294661b9e83c
Adam Balan <a-kyle@hotmail.com>
2023-10-07 10:38:58 -0600

    Trying to see if the original set up would work with nginx

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -40,8 +40,6 @@ return [
                 'host'   => '127.0.0.1',
                 'port'   => 6001,
                 'scheme' => env('APP_ENV') === 'local' ? 'http' : 'https',
-                'useTLS' => env('APP_ENV') === 'local' ? false : true,
-                'encrypted' => env('APP_ENV') === 'local' ? false : true,
             ],
         ],
 
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -53,7 +53,6 @@ window.Echo = new Echo({
   wsHost: window.location.hostname,
   wsPort: 6001,
   wssPort: 6001,
-  forceTLS: true,
   enabledTransports: ['ws', 'wss'],
   namespace: 'App',
   auth: {
