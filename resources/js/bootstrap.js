47a23d64abcd9f5fb5eab11b72dc812dcfbcd39d
Adam Balan <a-kyle@hotmail.com>
2023-10-07 22:15:24 -0600

    Lets put it all back to normal

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -45,19 +45,19 @@ Alpine.start();
  * allows your team to easily build robust real-time web applications.
  */
 
-// window.Pusher = require('pusher-js');
+window.Pusher = require('pusher-js');
 
-// window.Echo = new Echo({
-//   broadcaster: 'pusher',
-//   key: process.env.MIX_PUSHER_APP_KEY,
-//   wsHost: window.location.hostname,
-//   wsPort: 6001,
-//   wssPort: 6001,
-//   enabledTransports: ['ws', 'wss'],
-//   namespace: 'App',
-//   auth: {
-//     headers: {
-//       'X-CSRF-TOKEN': token.content
-//     }
-//   }
-// });
+window.Echo = new Echo({
+  broadcaster: 'pusher',
+  key: process.env.MIX_PUSHER_APP_KEY,
+  wsHost: window.location.hostname,
+  wsPort: 6001,
+  wssPort: 6001,
+  enabledTransports: ['ws', 'wss'],
+  namespace: 'App',
+  auth: {
+    headers: {
+      'X-CSRF-TOKEN': token.content
+    }
+  }
+});
