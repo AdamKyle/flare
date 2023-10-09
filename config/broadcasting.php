893ecab30673d155ec453dc6489a30d8320771aa
Adam Balan <a-kyle@hotmail.com>
2023-10-09 09:23:00 -0600

    Updated broadcasting

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,9 +37,9 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host'   => '127.0.0.1',
+                'host'   => env('PUSHER_HOST', '127.0.0.1'),
                 'port'   => 6001,
-                'scheme' => 'http'
+                'scheme' => env('PUSHER_PROTOCOL', 'http'),
             ],
         ],
 
