714dd8c69d208e776b8760be8ec5c7f43f1bbdcd
Adam Balan <a-kyle@hotmail.com>
2023-10-09 22:33:35 -0600

    reverting

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host'   => 'planesoftlessa.com',
+                'host'   => '127.0.0.1',
                 'port'   => 6001,
                 'scheme' => env('PUSHER_PROTOCOL', 'http')
             ],
