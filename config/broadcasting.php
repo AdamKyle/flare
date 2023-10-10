c9e8ffbb001039acb320ad1fa430a2399f21ef5f
Adam Balan <a-kyle@hotmail.com>
2023-10-09 23:06:52 -0600

    Not sure what to do

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,9 +37,9 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host'   => '127.0.0.1',
-                'port'   => 6001,
-                'scheme' => env('PUSHER_PROTOCOL', 'http'),
+                'host'    => env('PUSHER_HOST', '127.0.0.1'),
+                'port'    => 6001,
+                'scheme'  => env('PUSHER_PROTOCOL', 'http'),
             ],
         ],
 
