197346cf5971217bad4d6d1c074a55359e374034
Adam Balan <a-kyle@hotmail.com>
2023-10-09 22:43:26 -0600

    trying something

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,7 +39,11 @@ return [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'host'   => '127.0.0.1',
                 'port'   => 6001,
-                'scheme' => env('PUSHER_PROTOCOL', 'http')
+                'scheme' => env('PUSHER_PROTOCOL', 'http'),
+                'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ]
             ],
         ],
 
