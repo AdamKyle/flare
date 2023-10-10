2a4d0da5f639de17e6cdc017d8eccccd7b943d3f
Adam Balan <a-kyle@hotmail.com>
2023-10-09 22:49:28 -0600

    Not sure what to do

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -40,10 +40,6 @@ return [
                 'host'   => '127.0.0.1',
                 'port'   => 6001,
                 'scheme' => env('PUSHER_PROTOCOL', 'http'),
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => 0,
-                    CURLOPT_SSL_VERIFYPEER => 0,
-                ]
             ],
         ],
 
