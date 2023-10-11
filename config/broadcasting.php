cac78f50245777b866bac164ad93983ac728dc29
Adam Balan <a-kyle@hotmail.com>
2023-10-10 22:53:26 -0600

    Minor changes - lets hope this breaks nothing

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,14 +37,10 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
+                'encrypted' => env('APP_ENV') !== 'local',
                 'host'    => env('PUSHER_HOST', '127.0.0.1'),
                 'port'    => 6001,
                 'scheme'  => env('PUSHER_PROTOCOL', 'http'),
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => 0,
-                    CURLOPT_SSL_VERIFYPEER => 0,
-                ],
             ],
         ],
 
