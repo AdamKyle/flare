23ea122385f4018e0296643883e01efeb04e45d2
Adam Balan <a-kyle@hotmail.com>
2024-05-28 17:10:59 -0600

    minor changes

--- resources/js/game/sections/faction-loyalty/types/faction-npc-section-props.tsx
+++
@@ -1,7 +0,0 @@
-import { FactionLoyaltyNpc } from "../deffinitions/faction-loaylaty";
-
-export default interface FactionNpcSectionProps {
-    faction_loyalty_npc: FactionLoyaltyNpc;
-    can_craft: boolean;
-    can_attack: boolean;
-}
