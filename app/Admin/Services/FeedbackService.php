012d5faeeb7087877070fb53912ce0d4e160a3aa
Adam Balan <a-kyle@hotmail.com>
2024-08-22 14:17:00 -0600

    Minor changes to how the % shows for the bugs and suggestions

--- app/Admin/Services/FeedbackService.php
+++ app/Admin/Services/FeedbackService.php
@@ -11,31 +11,35 @@ class FeedbackService {
         $today = now()->startOfDay();
         $yesterday = now()->subDay()->startOfDay();
 
+        $totalBugCount = SuggestionAndBugs::where('type', FeedbackType::BUG)->count();
+        $totalSuggestionCount = SuggestionAndBugs::where('type', FeedbackType::SUGGESTION)->count();
+
         $bugsCountToday = SuggestionAndBugs::where('type', FeedbackType::BUG)->where('created_at', '>=', $today)->count();
         $bugsCountYesterday = SuggestionAndBugs::where('type', FeedbackType::BUG)->where('created_at', '>=', $yesterday)->where('created_at', '<', $today)->count();
 
         $suggestionCountToday = SuggestionAndBugs::where('type', FeedbackType::SUGGESTION)->where('created_at', '>=', $today)->count();
         $suggestionCountYesterday = SuggestionAndBugs::where('type', FeedbackType::SUGGESTION)->where('created_at', '>=', $yesterday)->where('created_at', '<', $today)->count();
 
-        $bugsDifference = $this->calculateDifference($bugsCountToday, $bugsCountYesterday);
-        $suggestionDifference = $this->calculateDifference($suggestionCountToday, $suggestionCountYesterday);
+        $bugsDifference = $this->calculatePercentageDifference($bugsCountToday, $bugsCountYesterday);
+        $suggestionDifference = $this->calculatePercentageDifference($suggestionCountToday, $suggestionCountYesterday);
+
 
         return [
-            'bugsCount' => $bugsCountToday,
-            'bugsDifference' => $bugsDifference,
-            'suggestionCount' => $suggestionCountToday,
-            'suggestionDifference' => $suggestionDifference,
+            'bugsCount' => $totalBugCount,
+            'bugsDifference' => abs($bugsDifference),
+            'suggestionCount' => $totalSuggestionCount,
+            'suggestionDifference' => abs($suggestionDifference),
         ];
-
     }
 
-    private function calculateDifference($todayCount, $yesterdayCount): float {
-        if ($yesterdayCount == 0) {
-            return $todayCount == 0 ? 0 : 100;
+    private function calculatePercentageDifference(int $countToday, int $countYesterday): float {
+        if ($countYesterday === 0) {
+            return $countToday > 0 ? ($countToday / 100) : 0.0;
         }
 
-        $difference = (($todayCount - $yesterdayCount) / $yesterdayCount) * 100;
+        $difference = $countToday - $countYesterday;
+        $percentageDifference = ($difference / $countYesterday) * 100;
 
-        return round($difference, 2);
+        return $percentageDifference;
     }
 }
