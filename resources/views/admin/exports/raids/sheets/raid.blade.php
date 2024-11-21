8e37831f7ee42f3d517a18ff4238b607e296b664
Adam Balan <a-kyle@hotmail.com>
2024-11-21 12:26:53 -0700

    Updated raid exports and raids

--- resources/views/admin/exports/raids/sheets/raid.blade.php
+++ resources/views/admin/exports/raids/sheets/raid.blade.php
@@ -8,6 +8,7 @@
             <th>raid_monster_ids</th>
             <th>raid_boss_location_id</th>
             <th>corrupted_location_ids</th>
+            <th>scheduled_event_description</th>
             <th>item_specialty_reward_type</th>
             <th>artifact_item_id</th>
         </tr>
@@ -22,6 +23,7 @@
             <td>{{!is_null($raid->raid_monster_ids) ? implode(',', \App\Flare\Models\Monster::whereIn('id', $raid->raid_monster_ids)->pluck('name')->toArray()) : ''}}
             <td>{{!is_null($raid->raid_boss_location_id) ? $raid->raidBossLocation->name : ''}}</td>
             <td>{{!is_null($raid->corrupted_location_ids) ? implode(',', \App\Flare\Models\Location::whereIn('id', $raid->corrupted_location_ids)->pluck('name')->toArray()) : ''}}</td>
+            <td>{{$raid->scheduled_event_description}}</td>
             <td>{{$raid->item_specialty_reward_type}}</td>
             <td>{{!is_null($raid->artifact_item_id) ? $raid->artifactItem->name : ''}}</td>
         </tr>
