d9e5d58d7076349ff7616911dc892c68f00a4e7b
Adam Balan <a-kyle@hotmail.com>
2025-04-04 20:19:44 -0400

    Adjustments

--- resources/js/ui/side-peek/side-peek.tsx
+++ resources/js/ui/side-peek/side-peek.tsx
@@ -16,24 +16,33 @@ const SidePeek = (props: SidePeekProps) => {
     <>
       {props.is_open && (
         <div
-          className="fixed inset-0 z-40 bg-black bg-opacity-50"
+          className="fixed inset-0 z-40 bg-black bg-opacity-50 dark:bg-black/70"
           onClick={handleClickingOutSide}
         />
       )}
       <div
         className={clsx(
-          'fixed top-0 right-0 h-full bg-white shadow-lg z-50 transition-transform duration-300 ease-in-out',
+          'fixed top-0 right-0 h-full bg-white dark:bg-gray-800 shadow-lg z-50 transition-transform duration-300 ease-in-out',
           props.is_open ? 'translate-x-0' : 'translate-x-full',
           'w-full sm:w-1/4'
         )}
       >
-        <div className="flex justify-between items-center p-4 border-b">
-          <h2 className="text-lg font-semibold">Side Peek</h2>
-          <button onClick={props.on_close}>
-            <i className="fa-solid fa-times text-xl text-gray-700"></i>
-          </button>
+        <div className="flex justify-between items-center p-4 border-b dark:border-gray-700">
+          <div className="flex items-center gap-2">
+            <button
+              onClick={props.on_close}
+              className="text-gray-700 dark:text-white px-2 py-1 hover:bg-gray-100 dark:hover:bg-gray-700 rounded"
+            >
+              <i className="fas fa-angle-double-right"></i>
+            </button>
+            <h2 className="text-lg font-semibold text-gray-900 dark:text-white">
+              Side Peek
+            </h2>
+          </div>
         </div>
-        <div className="p-4">{props.children}</div>
+        <div className="p-4 text-gray-900 dark:text-gray-100">
+          {props.children}
+        </div>
       </div>
     </>
   );
