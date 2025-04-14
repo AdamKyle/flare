952f922b5b92896bd261f7f8dce13da3dde840a8
Adam Balan <a-kyle@hotmail.com>
2025-04-14 16:37:19 -0400

    Cleanup

--- resources/js/game/components/side-peeks/base/component-registration/side-peek-component-props-map.ts
+++ resources/js/game/components/side-peeks/base/component-registration/side-peek-component-props-map.ts
@@ -1,7 +1,7 @@
 import { SidePeekComponentRegistrationEnum } from './side-peek-component-registration-enum';
 import BackPackProps from '../../character-inventory/backpack/types/backpack-props';
 import GemBagProps from '../../character-inventory/gem-bag/types/gem-bag-props';
-import UsableItemsProps from "../../character-inventory/usable-items/types/usable-items-props";
+import UsableItemsProps from '../../character-inventory/usable-items/types/usable-items-props';
 
 export type SidePeekComponentPropsMap = {
   [SidePeekComponentRegistrationEnum.BACKPACK]: BackPackProps;
