63546dfc21ab38e876a72999c8790882efe71d7d
Adam Balan <a-kyle@hotmail.com>
2025-04-23 09:29:24 -0400

    Trying a prefix to fix a failing queue

--- config/database.php
+++ config/database.php
@@ -230,6 +230,7 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
+            'prefix' => env('REDIS_PREFIX', 'planesoftlessa_database_'),
             'database' => 11,
         ],
 
