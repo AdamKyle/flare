b7490ee47a612585c09c39a887b4628281ac16bc
Adam Balan <a-kyle@hotmail.com>
2025-06-07 10:58:04 -0400

    Cleanup

--- resources/js/ui/modal/types/modal-props.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default interface ModalProps {
-  is_open: boolean;
-  title: string;
-  on_close?: () => void;
-  allow_clicking_outside?: boolean;
-  children?: React.ReactNode;
-}
