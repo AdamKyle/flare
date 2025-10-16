975f572e0061766682970b06d357a4ad88ff1792
Adam Balan <a-kyle@hotmail.com>
2025-10-16 19:33:25 -0400

    Fixed the loader on mobile.

--- resources/js/ui/container/container-wrapper.tsx
+++ resources/js/ui/container/container-wrapper.tsx
@@ -4,7 +4,7 @@ import ContainerWrapperProps from 'ui/container/types/container-wrapper-props';
 
 const ContainerWrapper = (props: ContainerWrapperProps): ReactNode => {
   return (
-    <div className="w-full mt-4 xl:mt-0 xl:w-2/3 xl:mx-auto xl:px-4 xl:py-6">
+    <div className="w-full mt-4 px-4 py-6 xl:mt-0 xl:w-2/3 xl:mx-auto">
       {props.children}
     </div>
   );
