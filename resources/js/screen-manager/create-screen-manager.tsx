d15a67fbe6aee14236d2e93681a33573d966138b
Adam Balan <a-kyle@hotmail.com>
2025-10-29 10:00:17 -0400

    Use the animation file

--- resources/js/screen-manager/create-screen-manager.tsx
+++ resources/js/screen-manager/create-screen-manager.tsx
@@ -17,25 +17,7 @@ import { ScreenMap } from './types/screen-map-type';
 import { ScreenName } from './types/screen-name-type';
 import { ScreenPropsOf } from './types/screen-props-of-type';
 import { StackEntry } from './types/stack-entry-type';
-
-const slideVariants = {
-  hidden: { x: '100%', opacity: 0 },
-  enter: {
-    x: 0,
-    opacity: 1,
-    transition: {
-      x: { type: 'tween', ease: 'easeOut', duration: 0.45 },
-      opacity: { duration: 0.2 },
-    },
-  },
-  exit: {
-    x: 0,
-    opacity: 0,
-    transition: {
-      opacity: { duration: 0.25, ease: 'easeIn' },
-    },
-  },
-};
+import {slideVariants} from "./variants/animation-variants";
 
 const createScreenManager = <TMap extends ScreenMap>() => {
   const Ctx = createContext<ScreenNavigation<TMap> | null>(null);
