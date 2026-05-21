385bff27527825a63cc661aee21a096b04e9afdd
Adam Balan <a-kyle@hotmail.com>
2026-05-21 14:00:28 -0400

    Fixed survey results page

---
+++ tests/Feature/Http/Controllers/SurveyStatsControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Feature\Http\Controllers;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class SurveyStatsControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testSurveyStatsReturnsSuccessfulResponseWhenNoSnapshotExists(): void
+    {
+        $this->visit('/survey-stats');
+
+        $this->assertResponseOk();
+        $this->assertViewHas('surveyExists', false);
+    }
+}
