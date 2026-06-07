465f925639831a282b5b2776291b9de27da01d50
Adam Balan <a-kyle@hotmail.com>
2026-06-07 18:55:16 -0400

    Fixed life stealing capping at 50% for vampires

--- tests/Unit/Flare/ServerFight/Fight/CharacterAttacks/SecondaryAttacksTest.php
+++ tests/Unit/Flare/ServerFight/Fight/CharacterAttacks/SecondaryAttacksTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Unit\Flare\ServerFight\Fight\CharacterAttacks;
 
+use App\Flare\Models\Location;
 use App\Flare\ServerFight\Fight\CharacterAttacks\SecondaryAttacks;
 use App\Flare\ServerFight\Monster\ServerMonster;
 use App\Flare\Values\AttackTypeValue;
+use App\Flare\Values\LocationType;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Cache;
 use Tests\Setup\Character\CharacterFactory;
@@ -56,6 +58,61 @@ class SecondaryAttacksTest extends TestCase
             'message' => 'The enemy resisted your attempt to steal 50.00% of their health and instead you stole 25.00%, dealing 250 damage.',
             'type' => 'enemy-action',
         ], $secondaryAttacks->getMessages());
+    }
+
+    public function testVampireAtUnderwaterCavesCanLifeStealSeventyFivePercent(): void
+    {
+        $character = (new CharacterFactory)->createBaseCharacter([], [
+            'name' => 'Vampire',
+        ])->givePlayerLocation()->getCharacter();
+        $character->map->x_position = 16;
+        $character->map->y_position = 16;
+
+        Location::create([
+            'name' => 'Underwater Caves',
+            'game_map_id' => $character->map->game_map_id,
+            'can_players_enter' => true,
+            'can_auto_battle' => true,
+            'description' => 'sample',
+            'is_port' => false,
+            'x' => 16,
+            'y' => 16,
+            'type' => LocationType::UNDERWATER_CAVES,
+        ]);
+
+        Cache::put('character-sheet-' . $character->id, [
+            'level' => $character->level,
+            'health' => 1000,
+        ]);
+
+        $monster = $this->createMonster([
+            'affix_resistance' => 0.0,
+            'life_stealing_resistance' => 0.0,
+            'is_raid_monster' => false,
+            'is_raid_boss' => false,
+        ]);
+
+        $serverMonster = (new ServerMonster)->setHealth(1000)->setMonster($monster->toArray());
+        $secondaryAttacks = resolve(SecondaryAttacks::class);
+        $secondaryAttacks->setCharacterHealth(250);
+        $secondaryAttacks->setMonsterHealth(1000);
+        $secondaryAttacks->setAttackData([
+            'attack_type' => AttackTypeValue::ATTACK,
+            'damage_deduction' => 0.0,
+            'affixes' => [
+                'cant_be_resisted' => true,
+                'stacking_life_stealing' => 0.75,
+                'life_stealing' => 0.0,
+            ],
+            'weapon_damage' => 100,
+            'special_damage' => [],
+            'ring_damage' => 0,
+        ]);
+
+        $secondaryAttacks->affixLifeStealingDamage($character, $serverMonster);
+
+        $this->assertEquals(250, $secondaryAttacks->getMonsterHealth());
+        $this->assertEquals(1000, $secondaryAttacks->getCharacterHealth());
     }
 
     public function testMonsterWithZeroLifeStealingResistanceDoesNotAddResistanceMessage(): void
